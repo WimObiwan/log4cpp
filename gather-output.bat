77b92e479049143c4d4a685f08764623ab292fd7
Wim Devos <Wim.Devos@meucci-solutions.com>
2012-12-13 14:44:15 +0100

    Added new batch file 'gather-output.bat' to simplify deployment

---
+++ gather-output.bat
@@ -0,0 +1,14 @@
+rd /Q /S output
+md output\include
+md output\bin
+md output\lib
+
+xcopy src\main\include output\include\ /S
+
+copy projects\Debug\log4cxx.dll output\bin\log4cxxd.dll
+copy projects\Debug\log4cxx.pdb output\bin\log4cxxd.pdb
+copy projects\Debug\log4cxx.lib output\lib\log4cxxd.lib
+
+copy projects\Release\log4cxx.dll output\bin\log4cxx.dll
+copy projects\Release\log4cxx.pdb output\bin\log4cxx.pdb
+copy projects\Release\log4cxx.lib output\lib\log4cxx.lib
