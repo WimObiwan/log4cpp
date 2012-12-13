096024f1687e241a59ab82963552c11bdf77f036
Wim Devos <Wim.Devos@meucci-solutions.com>
2012-12-13 15:19:42 +0100

    Changed output name of debug-versions to log4cxxD.* (with 'd' suffix)

--- gather-output.bat
+++ gather-output.bat
@@ -5,9 +5,9 @@ md output\lib
 
 xcopy src\main\include output\include\ /S
 
-copy projects\Debug\log4cxx.dll output\bin\log4cxxd.dll
-copy projects\Debug\log4cxx.pdb output\bin\log4cxxd.pdb
-copy projects\Debug\log4cxx.lib output\lib\log4cxxd.lib
+copy projects\Debug\log4cxxd.dll output\bin\log4cxxd.dll
+copy projects\Debug\log4cxxd.pdb output\bin\log4cxxd.pdb
+copy projects\Debug\log4cxxd.lib output\lib\log4cxxd.lib
 
 copy projects\Release\log4cxx.dll output\bin\log4cxx.dll
 copy projects\Release\log4cxx.pdb output\bin\log4cxx.pdb
